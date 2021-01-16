d3f9216ca8f6f2bc4e2ca057952df6de7c3dcf53
LJspice <8888722+LJspice@users.noreply.github.com>
2021-01-16 12:00:39 -0800

    Update mh400e_common.h
    Reduced number of gears

--- mh400e_common.h
+++ mh400e_common.h
@@ -50,32 +50,16 @@ typedef struct
 
 /* lookup table from rpm to gearbox status pin values */
 static pair_t mh400e_gears[] =
-{  /* rpm   bitmask                msb 11 10 9 8 7 6 5 4 3 2 1 0 lsb */
-    { 0,    4       },              /* neutral           0 1 0 0 */
-    { 80,   1097    },              /*   0 1 0 0 0 1 0 0 1 0 0 1 */
-    { 100,  2377    },              /*   1 0 0 1 0 1 0 0 1 0 0 1 */
-    { 125,  585     },              /*   1 0 1 0 0 1 0 0 1 0 0 1 */
-    { 160,  1177    },              /*   0 1 0 0 1 0 0 1 1 0 0 1 */
-    { 200,  2457    },              /*   1 0 0 1 1 0 0 1 1 0 0 1 */
-    { 250,  665     },              /*   0 0 1 0 1 0 0 1 1 0 0 1 */
-    { 315,  1065    },              /*   0 1 0 0 0 0 1 0 1 0 0 1 */
-    { 400,  2345    },              /*   1 0 0 1 0 0 1 0 1 0 0 1 */
-    { 500,  553     },              /*   0 0 1 0 0 0 1 0 1 0 0 1 */
-    { 630,  1090    },              /*   0 1 0 0 0 1 0 0 0 0 1 0 */
-    { 800,  2370    },              /*   1 0 0 1 0 1 0 0 0 0 1 0 */
-    { 1000, 578     },              /*   0 0 1 0 0 1 0 0 0 0 1 0 */
-    { 1250, 1170    },              /*   0 1 0 0 1 0 0 1 0 0 1 0 */
-    { 1600, 2450    },              /*   1 0 0 1 1 0 0 1 0 0 1 0 */
-    { 2000, 658     },              /*   0 0 1 0 1 0 0 1 0 0 1 0 */
-    { 2500, 1058    },              /*   0 1 0 0 0 0 1 0 0 0 1 0 */
-    { 3150, 2338    },              /*   1 0 0 1 0 0 1 0 0 0 1 0 */
-    { 4000, 546     }               /*   0 0 1 0 0 0 1 0 0 0 1 0 */
+{  /* gear   bitmask                msb 3 2 1 0 lsb */
+    { 0,    4       },             /* neutral       0 1 0 0 */
+    { 1,    1097    },             /* low gear      1 0 0 1 */
+    { 2,    2377    },             /* high gear     1 0 0 1 */
 };
 
 /* Furthest CCW position, marked as "red" on the MAHO   */
 #define MH400E_STAGE_POS_LEFT               9   /* 1001 */
 
-/* Middle shaft position, marked as "bloue" on the MAHO */
+/* Middle shaft position, marked as "blue" on the MAHO */
 #define MH400E_STAGE_POS_CENTER             4   /* 0100 */
 
 /* Furthest CW position, marked as "yellow" on the MAHO */
@@ -88,11 +72,11 @@ static pair_t mh400e_gears[] =
 /* index of neutral gear */
 #define MH400E_NEUTRAL_GEAR_INDEX   0
 /* min spindle rpm > 0 supported by the MH400E */
-#define MH400E_MIN_RPM              80
+#define MH400E_MIN_RPM              1
 /* index of the first non 0 rpm setting in the gears array */
 #define MH400E_MIN_RPM_INDEX        1
 /* max spindle rpm supported by the MH400E */
-#define MH400E_MAX_RPM              4000
+#define MH400E_MAX_RPM              2
 
 #define MH400E_TWITCH_KEEP_PIN_ON   800*1000000L /* 800ms in nanoseconds */
 #define MH400E_TWITCH_KEEP_PIN_OFF  200*1000000L /* 200ms in nanoseconds */
